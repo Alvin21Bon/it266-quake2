eb180731526a5ba62786d4e24bed7a0467310ca1
Alvin21Bon <alvin21bon@gmail.com>
2023-03-31 03:31:36 -0400

    Decided that the mod will be in its own folder instead of editing baseq2. Made changes to reflect that

--- apply-mod.sh
+++ apply-mod.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#   1)  COMPLETELY WIPE THE TARGET GAME DIRECTORY
-#   2)  COPY THE BASEFILES INTO THE TARGET GAME
-#   3)  COPY THE BINARIES INTO THE TARGET GAME
-#   4)  COPY THE MOD FILES INTO THE TAGRET GAME (WIPING BASE FILES/FOLDERS IF NECESSARY)
-#           (NO WARNING IF FILE/FOLDER DOES NOT EXIST)
+#   1)  COMPLETELY WIPE THE TARGET MOD DIRECTORY
+#   2)  COPY THE BINARIES INTO THE TARGET GAME AND MOD
+#   3)  COPY THE MOD FILES INTO THE TAGRET MOD
 
 
 target_game_dir="../game-linux"
-basefiles_dir="../game-basefiles"
+basefiles_dir="../game-basefiles/baseq2"
 mod_binaries_dir="release"
 mod_files_dir="mod-files"
 
-
-#*****************************************************
-#   1)  COMPLETELY WIPE THE TARGET GAME DIRECTORY
-#*****************************************************
